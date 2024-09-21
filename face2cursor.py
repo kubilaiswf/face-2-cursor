8615e9d813092a988bab2df1cad78ec4fe82aab3
kubilay <41905346+kubilaiswf@users.noreply.github.com>
2024-09-21 19:40:46 +0300

    added customtkinker and functionality
    i aimed for a better user interface and tried to make the cursor move only when the nose coordinate gains or loses 3 values in order to smooth the cursor movement.

--- face2cursor.py
+++ face2cursor.py
@@ -8,7 +8,12 @@ import numpy as np
 from scipy.ndimage import gaussian_filter1d
 import time
 import threading
+import customtkinter
 from concurrent.futures import ThreadPoolExecutor
+
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
 
 warnings.filterwarnings("ignore", category=UserWarning, module='google')
 
@@ -27,7 +32,7 @@ blink_threshold = 0.15
 blink_detected_left = False
 blink_detected_right = False
 
-window = tk.Tk()
+window = customtkinter.CTk()
 window.title("face2cursor :P")
 window.geometry("1200x1000")
 
@@ -40,7 +45,7 @@ ui_texts = {
     "acceleration_y": "Acceleration Y",
     "left_eye": "Left Eye",
     "right_eye": "Right Eye",
-    "preferred_eye": "Preferred Eye:",
+    "preferred_eye": "Preferred Eye",
     "fps": "FPS",
     "nose_x": "Nose X",
     "nose_y": "Nose Y",
@@ -48,18 +53,17 @@ ui_texts = {
     "right_eye_ratio": "Right Eye Ratio"
 }
 
-mirror_image = tk.IntVar(value=1)
+mirror_image = customtkinter.IntVar(value=1)
 sensitivity_x = 1
 sensitivity_y = 1
 acceleration_x = 1
 acceleration_y = 1
-blink_eye = tk.IntVar(value=1)  # 1: Sol göz, 2: Sağ göz
+blink_eye = customtkinter.IntVar(value=1)  # 1: Left eye, 2: Right eye
 last_update_time = time.time()
 
-# ThreadPoolExecutor kullanımı
 executor = ThreadPoolExecutor(max_workers=4)
 
-# Göz kırpma işlemini ayrı bir thread'e taşıma fonksiyonu
+
 def check_blink(left_eye_aspect_ratio, right_eye_aspect_ratio, blink_eye):
     global blink_detected_left, blink_detected_right
 
@@ -67,44 +71,51 @@ def check_blink(left_eye_aspect_ratio, right_eye_aspect_ratio, blink_eye):
         blink_detected_left = False
         blink_detected_right = False
     else:
-        if blink_eye.get() == 1:  # Sol göz
+        if blink_eye.get() == 1:
             if left_eye_aspect_ratio < blink_threshold and not blink_detected_left:
                 blink_detected_left = True
                 pyautogui.click()
             elif left_eye_aspect_ratio >= blink_threshold:
                 blink_detected_left = False
-        else:  # Sağ göz
+        else:
             if right_eye_aspect_ratio < blink_threshold and not blink_detected_right:
                 blink_detected_right = True
                 pyautogui.click()
             elif right_eye_aspect_ratio >= blink_threshold:
                 blink_detected_right = False
 
-# Hareket ve hassasiyet değişkenlerini güncelleme fonksiyonları
+
 def update_sensitivity_x(val):
     global sensitivity_x
     sensitivity_x = float(val)
+    lbl_sensitivity_x_value.configure(text=f'Sensitivity X = {val:.2f}')
+
 
 def update_sensitivity_y(val):
     global sensitivity_y
     sensitivity_y = float(val)
+    lbl_sensitivity_y_value.configure(text=f'Sensitivity Y = {val:.2f}')
+
 
 def update_acceleration_x(val):
     global acceleration_x
     acceleration_x = float(val)
+    lbl_acceleration_x_value.configure(text=f'Acceleration X = {val:.2f}')
+
 
 def update_acceleration_y(val):
     global acceleration_y
     acceleration_y = float(val)
+    lbl_acceleration_y_value.configure(text=f'Acceleration Y = {val:.2f}')
 
-# OpenCV görüntüsünü Tkinter görüntüsüne çevirme fonksiyonu
+
 def convert_to_tk_image(cv_image):
     cv_image = cv2.resize(cv_image, (600, 500))
     rgb_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
     pil_image = Image.fromarray(rgb_image)
     return ImageTk.PhotoImage(pil_image)
 
-# Göz kırpma oranını hesaplama fonksiyonu
+
 def calculate_eye_aspect_ratio(eye_landmarks, frame_width, frame_height):
     point = lambda i: np.array([eye_landmarks[i].x * frame_width, eye_landmarks[i].y * frame_height])
     horizontal_distance = np.linalg.norm(point(0) - point(3))
@@ -112,35 +123,18 @@ def calculate_eye_aspect_ratio(eye_landmarks, frame_width, frame_height):
     vertical_distance_2 = np.linalg.norm(point(2) - point(4))
     return (vertical_distance_1 + vertical_distance_2) / (2.0 * horizontal_distance)
 
-# FPS sınırlaması
+
 fps_limit = 50
 prev_frame_time = 0
 
-# Göz ve burun landmark'larını çizdiren fonksiyon
+
 def draw_selected_landmarks(frame, face_landmarks, frame_width, frame_height):
-    # Sol göz landmark'ları
-    left_eye_indices = [33, 160, 158, 133, 153, 144]
-    # Sağ göz landmark'ları
-    right_eye_indices = [362, 385, 387, 263, 373, 380]
-    # Burun landmark'ı
-    nose_indices = [4]
+    pass
 
-    # Belirtilen landmarkları çizdirme
 
-    #for i in left_eye_indices:
-        #landmark = face_landmarks.landmark[i]
-        #cv2.circle(frame, (int(landmark.x * frame_width), int(landmark.y * frame_height)), 4, (70, 0, 130), -1)
-
-    #for i in right_eye_indices:
-        #landmark = face_landmarks.landmark[i]
-        #cv2.circle(frame, (int(landmark.x * frame_width), int(landmark.y * frame_height)), 4, (70, 0, 130), -1)
-
-    #for i in nose_indices:
-        #landmark = face_landmarks.landmark[i]
-        #cv2.circle(frame, (int(landmark.x * frame_width), int(landmark.y * frame_height)), 4, (255, 140, 0), -1)
-
-# Ana görüntü işleme döngüsü
 fps = 0
+
+
 def show_frame():
     global nose_positions, sensitivity_x, sensitivity_y, acceleration_x, acceleration_y, blink_eye, fps, cap, prev_frame_time, last_update_time
 
@@ -150,7 +144,7 @@ def show_frame():
         return
 
     current_time = time.time()
-    if current_time - prev_frame_time < 1.0 / fps_limit:  # FPS sınırlaması
+    if current_time - prev_frame_time < 1.0 / fps_limit:
         window.after(10, show_frame)
         return
 
@@ -169,28 +163,28 @@ def show_frame():
             nose_x = int(nose_tip.x * frame_width)
             nose_y = int(nose_tip.y * frame_height)
 
-            nose_positions['x'].append(nose_x)
-            nose_positions['y'].append(nose_y)
-
             if len(nose_positions['x']) > 5:
                 nose_positions['x'].pop(0)
                 nose_positions['y'].pop(0)
 
+            nose_positions['x'].append(nose_x)
+            nose_positions['y'].append(nose_y)
+
             avg_nose_x = np.mean(gaussian_filter1d(nose_positions['x'], sigma=2))
             avg_nose_y = np.mean(gaussian_filter1d(nose_positions['y'], sigma=2))
 
-            delta_x = (avg_nose_x - nose_x) * sensitivity_x * acceleration_x
-            delta_y = (avg_nose_y - nose_y) * sensitivity_y * acceleration_y
+            if len(nose_positions['x']) >= 3 and len(nose_positions['y']) >= 3:
+                delta_x = (avg_nose_x - nose_x) * sensitivity_x * acceleration_x
+                delta_y = (avg_nose_y - nose_y) * sensitivity_y * acceleration_y
 
-            delta_x = -delta_x
-            delta_y = -delta_y
+                delta_x = -delta_x
+                delta_y = -delta_y
 
-            current_mouse_x, current_mouse_y = pyautogui.position()
-            new_mouse_x = current_mouse_x + delta_x / 5
-            new_mouse_y = current_mouse_y + delta_y / 5
+                current_mouse_x, current_mouse_y = pyautogui.position()
+                new_mouse_x = current_mouse_x + delta_x / 5
+                new_mouse_y = current_mouse_y + delta_y / 5
 
-            # PyAutoGUI işlemini ThreadPoolExecutor ile yap
-            executor.submit(pyautogui.moveTo, new_mouse_x, new_mouse_y)
+                executor.submit(pyautogui.moveTo, new_mouse_x, new_mouse_y)
 
             left_eye_indices = [33, 160, 158, 133, 153, 144]
             right_eye_indices = [362, 385, 387, 263, 373, 380]
@@ -201,21 +195,17 @@ def show_frame():
             left_eye_aspect_ratio = calculate_eye_aspect_ratio(left_eye_landmarks, frame_width, frame_height)
             right_eye_aspect_ratio = calculate_eye_aspect_ratio(right_eye_landmarks, frame_width, frame_height)
 
-            # Göz kırpma işlemini thread'e al
             executor.submit(check_blink, left_eye_aspect_ratio, right_eye_aspect_ratio, blink_eye)
-
-            # Sadece göz ve burun landmark'larını çizdir
             executor.submit(draw_selected_landmarks, frame, face_landmarks, frame_width, frame_height)
 
-            # Ekrana yazılar ekle
             cv2.putText(frame, f'{ui_texts["left_eye_ratio"]}: {left_eye_aspect_ratio:.2f}', (10, 30),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             cv2.putText(frame, f'{ui_texts["right_eye_ratio"]}: {right_eye_aspect_ratio:.2f}', (10, 60),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             cv2.putText(frame, f'{ui_texts["nose_x"]}: {avg_nose_x:.2f}', (10, 90),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 255, 0), 2)
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             cv2.putText(frame, f'{ui_texts["nose_y"]}: {avg_nose_y:.2f}', (10, 120),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 255, 0), 2)
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
             current_time = time.time()
             fps = 1 / (current_time - last_update_time)
@@ -224,56 +214,71 @@ def show_frame():
             cv2.putText(frame, f'{ui_texts["fps"]}: {fps:.2f}', (10, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     img_tk = convert_to_tk_image(frame)
-    lbl_video.imgtk = img_tk
-    lbl_video.configure(image=img_tk)
+    if img_tk:
+        lbl_video.configure(image=img_tk)
+        lbl_video.image = img_tk
 
     lbl_video.after(10, show_frame)
 
-# Kontrol paneli ve GUI bileşenleri
-frame_controls = tk.Frame(window, bd=2, relief=tk.SUNKEN)
-frame_controls.pack(side=tk.TOP, pady=10)
+
+frame_controls = customtkinter.CTkFrame(window)
+frame_controls.pack(side=customtkinter.TOP, pady=10)
 
 lbl_video = tk.Label(window)
 lbl_video.pack(pady=10)
 
-chk_mirror = tk.Checkbutton(frame_controls, text=ui_texts["mirror"], variable=mirror_image, bg="lightblue",
-                            font=("Helvetica", 14))
+chk_mirror = customtkinter.CTkCheckBox(frame_controls, text=ui_texts["mirror"], variable=mirror_image)
 chk_mirror.pack(pady=5, padx=10)
 
-sensitivity_slider_x = tk.Scale(frame_controls, from_=0.1, to=10.0, orient=tk.HORIZONTAL, resolution=0.1,
-                                label=ui_texts["sensitivity_x"], command=update_sensitivity_x)
+# Sensitivity X
+lbl_sensitivity_x_value = customtkinter.CTkLabel(frame_controls, text=f'Sensitivity X = {sensitivity_x:.2f}')
+lbl_sensitivity_x_value.pack(side=customtkinter.TOP, pady=5)
+
+sensitivity_slider_x = customtkinter.CTkSlider(frame_controls, from_=0.1, to=10.0, orientation="horizontal",
+                                               number_of_steps=100, command=update_sensitivity_x)
 sensitivity_slider_x.set(sensitivity_x)
-sensitivity_slider_x.pack(side=tk.LEFT, pady=5, padx=10)
+sensitivity_slider_x.pack(side=customtkinter.TOP, pady=5, padx=10)
 
-sensitivity_slider_y = tk.Scale(frame_controls, from_=0.1, to=10.0, orient=tk.HORIZONTAL, resolution=0.1,
-                                label=ui_texts["sensitivity_y"], command=update_sensitivity_y)
+# Sensitivity Y
+lbl_sensitivity_y_value = customtkinter.CTkLabel(frame_controls, text=f'Sensitivity Y = {sensitivity_y:.2f}')
+lbl_sensitivity_y_value.pack(side=customtkinter.TOP, pady=5)
+
+sensitivity_slider_y = customtkinter.CTkSlider(frame_controls, from_=0.1, to=10.0, orientation="horizontal",
+                                               number_of_steps=100, command=update_sensitivity_y)
 sensitivity_slider_y.set(sensitivity_y)
-sensitivity_slider_y.pack(side=tk.LEFT, pady=5, padx=10)
+sensitivity_slider_y.pack(side=customtkinter.TOP, pady=5, padx=10)
 
-acceleration_slider_x = tk.Scale(frame_controls, from_=0.1, to=5.0, orient=tk.HORIZONTAL, resolution=0.1,
-                                 label=ui_texts["acceleration_x"], command=update_acceleration_x)
+# Acceleration X
+lbl_acceleration_x_value = customtkinter.CTkLabel(frame_controls, text=f'Acceleration X = {acceleration_x:.2f}')
+lbl_acceleration_x_value.pack(side=customtkinter.TOP, pady=5)
+
+acceleration_slider_x = customtkinter.CTkSlider(frame_controls, from_=0.1, to=5.0, orientation="horizontal",
+                                                number_of_steps=100, command=update_acceleration_x)
 acceleration_slider_x.set(acceleration_x)
-acceleration_slider_x.pack(side=tk.LEFT, pady=5, padx=10)
+acceleration_slider_x.pack(side=customtkinter.TOP, pady=5, padx=10)
 
-acceleration_slider_y = tk.Scale(frame_controls, from_=0.1, to=5.0, orient=tk.HORIZONTAL, resolution=0.1,
-                                 label=ui_texts["acceleration_y"], command=update_acceleration_y)
+# Acceleration Y
+lbl_acceleration_y_value = customtkinter.CTkLabel(frame_controls, text=f'Acceleration Y = {acceleration_y:.2f}')
+lbl_acceleration_y_value.pack(side=customtkinter.TOP, pady=5)
+
+acceleration_slider_y = customtkinter.CTkSlider(frame_controls, from_=0.1, to=5.0, orientation="horizontal",
+                                                number_of_steps=100, command=update_acceleration_y)
 acceleration_slider_y.set(acceleration_y)
-acceleration_slider_y.pack(side=tk.LEFT, pady=5, padx=10)
+acceleration_slider_y.pack(side=customtkinter.TOP, pady=5, padx=10)
 
-lbl_preferred_eye = tk.Label(frame_controls, text=ui_texts["preferred_eye"], font=("Helvetica", 14))
-lbl_preferred_eye.pack(side=tk.LEFT, pady=5, padx=10)
+lbl_preferred_eye = customtkinter.CTkLabel(frame_controls, text=ui_texts["preferred_eye"])
+lbl_preferred_eye.pack(side=customtkinter.TOP, pady=5)
 
-radio_btn_left_eye = tk.Radiobutton(frame_controls, text=ui_texts["left_eye"], variable=blink_eye, value=1)
-radio_btn_left_eye.pack(side=tk.LEFT, pady=5, padx=10)
+radio_btn_left_eye = customtkinter.CTkRadioButton(frame_controls, text=ui_texts["left_eye"], variable=blink_eye,
+                                                  value=1)
+radio_btn_left_eye.pack(side=customtkinter.LEFT, pady=5, padx=10)
 
-radio_btn_right_eye = tk.Radiobutton(frame_controls, text=ui_texts["right_eye"], variable=blink_eye, value=2)
-radio_btn_right_eye.pack(side=tk.LEFT, pady=5, padx=10)
+radio_btn_right_eye = customtkinter.CTkRadioButton(frame_controls, text=ui_texts["right_eye"], variable=blink_eye,
+                                                   value=2)
+radio_btn_right_eye.pack(side=customtkinter.LEFT, pady=5, padx=10)
 
-with mp_face_mesh.FaceMesh(
-        max_num_faces=1,
-        refine_landmarks=True,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5) as face_mesh:
+with mp_face_mesh.FaceMesh(max_num_faces=1, refine_landmarks=True, min_detection_confidence=0.5,
+                           min_tracking_confidence=0.5) as face_mesh:
     show_frame()
     window.mainloop()
 
